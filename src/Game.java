986adb87fced2e080e42504c67247db7095d3266
Nicholas Kennedy <nmk10062@gmail.com>
2024-01-12 17:03:07 -0500

    Add thread to game class

--- src/Game.java
+++ src/Game.java
@@ -3,12 +3,18 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 
-public class Game extends JPanel {
+public class Game extends JPanel implements Runnable {
+
+    private final Thread thread;
+    private boolean running;
 
     public Game(int width, int height) {
         super(true);
 
         setPreferredSize(new Dimension(width, height));
+
+        this.thread = new Thread(this);
+        this.running = false;
     }
 
     public void paintComponent(Graphics g) {
@@ -19,4 +25,32 @@ public class Game extends JPanel {
         g.fillRect(0, 0, getWidth(), getHeight());
     }
 
+    public synchronized void start() {
+        if(running) {
+            return;
+        }
+
+        thread.start();
+        this.running = true;
+    }
+
+    public synchronized void stop() {
+        if(!this.running) {
+            return;
+        }
+
+        try {
+            thread.join();
+        } catch(InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+
+        this.running = false;
+        System.exit(0);
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Starting Game...");
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
         Window window = new Window("Breakout");
+        Game game = new Game(600, 800);
 
-        window.add(new Game(600, 800));
+        window.add(game);
         window.pack();
         window.setVisibility(true);
+        game.start();
     }
 }
